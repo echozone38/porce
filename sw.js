ec904ae9bb425c6dd6719a147b0a01402ac85ce7
echozone38 <echozone.38@gmail.com>
2025-12-27 13:17:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 // Service Worker pro "Reálná porce ze syrové směsi"
-const CACHE_NAME = 'real-syrova-porce-cache-v9';
+const CACHE_NAME = 'real-syrova-porce-cache-v10';
 const CORE_ASSETS = [ './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png' ];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE_ASSETS)));
